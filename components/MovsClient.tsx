cf26d0fadbdd482ca7c69a4a89ed3177a0621d4d
AntonioIonica <antonioionica@gmail.com>
2026-01-30 18:49:52 +0200

    feat: added formated local time

--- components/MovsClient.tsx
+++ components/MovsClient.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import moment from "moment-timezone";
+
 type MovsClientProps = {
     userId: string | undefined;
     paramsUserId: string;
@@ -26,8 +28,21 @@ type MovsClientProps = {
     }>
 }
 
+function parseLocalTime(timeData: string) {
+    const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    let currentTime = moment().tz(timezone).format(timeData);
+    const localDate = currentTime.split("T")[0];
+    const localHour = currentTime.split("T")[1];
+    currentTime = localHour.split(".")[0] + "-" + localDate;
+    return currentTime;
+}
+
 
 export default function MovsClient({ userId, paramsUserId, workouts }: MovsClientProps) {
+    function handleAccordion(e) {
+        return null
+    }
+
     if (userId !== paramsUserId) {
         return <div>You are not logged in!</div>
     }
@@ -44,13 +59,22 @@ export default function MovsClient({ userId, paramsUserId, workouts }: MovsClien
         {/* Container */}
         <div className="flex w-full h-[64vh]">
             <div className="border-2 border-solid border-cyan-100 w-[40%] h-full">
-                <div className="container flex flex-col items-start">
-                    {workouts.map((workout) => (<><button className="accordion">{workout.name}</button><div></div></>))}
+                <div className="container flex flex-col items-start w-full space-y-0">
+                    {workouts.map((workout, index) => (<div className="w-full border-2 border-solid 
+                    rounded-sm border-primary px-4 flex flex-col" key={index}>
+                        <button onClick={(e) => handleAccordion(e)}
+                            className="accordion w-full text-start">{workout.name}</button>
+                        <div className="panel flex flex-col ml-6 w-full">
+                            {/* Workout details */}
+                            <div className="flex w-full">
+                                <span>Duration: {workout.duration}</span>
+                                <span>Completed at: {parseLocalTime(workout.completed_at.toString())}</span>
+                            </div>
+                        </div>
+                    </div>))}
                 </div>
             </div>
             <div className="border-2 border-solid border-cyan-100 w-[60%] h-full">Charts</div>
-        </div>
-
-
+        </div >
     </div>
 }
