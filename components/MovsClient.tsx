5c3006e476b3ea4d560807e06821e148fda5361b
AntonioIonica <antonioionica@gmail.com>
2026-01-30 22:32:58 +0200

    feat: added accordion style to exercises

--- components/MovsClient.tsx
+++ components/MovsClient.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import moment from "moment-timezone";
+import { useState } from "react";
 
 type MovsClientProps = {
     userId: string | undefined;
@@ -39,13 +40,22 @@ function parseLocalTime(timeData: string) {
 
 
 export default function MovsClient({ userId, paramsUserId, workouts }: MovsClientProps) {
-    function handleAccordion(e) {
-        return null
-    }
+    const [openWorkout, setOpenWorkout] = useState<null | number>(null);
+    const [openExercise, setOpenExercise] = useState<null | number>(null);
+
+    function toggleWorkouts(index: number) {
+        setOpenWorkout(openWorkout === index ? null : index);
+    };
+
+    function toggleExercises(index: number) {
+        if (openWorkout !== null) {
+            setOpenExercise(openExercise === index ? null : index);
+        }
+    };
 
     if (userId !== paramsUserId) {
         return <div>You are not logged in!</div>
-    }
+    };
 
     return <div className="flex flex-col max-h-screen min-h-[78vh] w-screen px-10">
         {/* Streaks */}
@@ -58,22 +68,52 @@ export default function MovsClient({ userId, paramsUserId, workouts }: MovsClien
 
         {/* Container */}
         <div className="flex w-full h-[64vh]">
-            <div className="border-2 border-solid border-cyan-100 w-[40%] h-full">
+            {/* Workouts list */}
+            <div className="border-2 border-solid border-cyan-100 w-[40%] h-full max-h-[64vh] overflow-y-auto">
                 <div className="container flex flex-col items-start w-full space-y-0">
                     {workouts.map((workout, index) => (<div className="w-full border-2 border-solid 
                     rounded-sm border-primary px-4 flex flex-col" key={index}>
-                        <button onClick={(e) => handleAccordion(e)}
-                            className="accordion w-full text-start">{workout.name}</button>
-                        <div className="panel flex flex-col ml-6 w-full">
+                        <button onClick={() => toggleWorkouts(index)}
+                            className="accordion w-full text-start">{workout.name}
+                        </button>
+
+                        {/* Details of eah workout */}
+                        {openWorkout === index && (
+                            <div className="panel overflow-hidden flex-col w-full">
                             {/* Workout details */}
-                            <div className="flex w-full">
-                                <span>Duration: {workout.duration}</span>
+                                <div className="flex w-full space-x-2">
+                                    <span>Duration: {workout.duration} sec</span>
                                 <span>Completed at: {parseLocalTime(workout.completed_at.toString())}</span>
                             </div>
-                        </div>
+                                {/* Exercises details */}
+                                <div className="flex w-full flex-col">
+                                    <div className="flex flex-col">
+                                        {workout.exercises.map((exercises, index) => (<div key={index} className="flex flex-col w-full">
+                                            <div className="flex border-2 border-solid border-primary space-x-4">
+                                                <button onClick={() => toggleExercises(index)}>{exercises.name}</button>
+                                                <span>Notes: {exercises.notes}</span>
+                                            </div>
+                                            {openExercise === index && (
+                                                <div className="flex text-sm ml-4">{exercises.sets.map((set, index) => (
+                                                    <div key={index} className="flex">
+                                                        <span>No: {set.set_number}</span>
+                                                        <span>Reps: {set.reps}</span>
+                                                        <span>Duration: {set.duration}</span>
+                                                        <span>Rest time: {set.rest_time}</span>
+                                                        <span>Weight: {set.weight}</span>
+                                                        <span>RPE: {set.rpe}</span>
+                                                    </div>
+                                                ))}</div>
+                                            )}
+                                        </div>))}
+                                    </div>
+                                </div>
+                            </div>)}
                     </div>))}
                 </div>
             </div>
+
+            {/* Charts */}
             <div className="border-2 border-solid border-cyan-100 w-[60%] h-full">Charts</div>
         </div >
     </div>
